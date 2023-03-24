4473bda1926c6878f8d8ed3b58dc38d2bbd12650
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2023-03-24 07:34:10 +0000

    add pubsub function supposed to scrape page

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,13 @@ import * as functions from "firebase-functions";
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+  functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
+
+exports.scheduledFunction = functions.pubsub
+  .schedule("every 5 minutes")
+  .onRun((context) => {
+    console.log("This will be run every 5 minutes!");
+    return null;
+  });
