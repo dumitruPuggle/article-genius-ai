3648520920780ca4fc5641ccfcf1dd2ec8d99c96
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2023-03-24 07:21:50 +0000

    cleanup boilerplate

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
-import { useCallback, useEffect, useState } from 'react'
-import Button from '../components/Button'
-import ClickCount from '../components/ClickCount'
-import styles from '../styles/home.module.css'
-
-function throwError() {
-  console.log(
-    // The function body() is not defined
-    document.body()
-  )
-}
+import styles from "../styles/home.module.css";
 
 function Home() {
-  const [count, setCount] = useState(0)
-  const increment = useCallback(() => {
-    setCount((v) => v + 1)
-  }, [setCount])
-
-  useEffect(() => {
-    const r = setInterval(() => {
-      increment()
-    }, 1000)
-
-    return () => {
-      clearInterval(r)
-    }
-  }, [increment])
-
-  return (
-    <main className={styles.main}>
-      Hello world
-    </main>
-  )
+  return <main className={styles.main}>Hello world</main>;
 }
 
-export default Home
+export default Home;
