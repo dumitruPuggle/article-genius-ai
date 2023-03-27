a1ddcf8a9e298f0e7f696ab5b93b13c199bdef2b
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2023-03-27 22:46:14 +0300

    regenerate using davinci-003 model

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import { Configuration, OpenAIApi } from "openai";
 import { scrapeGamespot } from "./core/scrapers/gamespot/scrape-gamespot";
 
 // // Start writing functions
@@ -8,6 +9,22 @@ exports.scrape = functions
   .runWith({ timeoutSeconds: 120, memory: "2GB" })
   .region("us-central1")
   .https.onRequest(async (request, response) => {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY ?? "",
+    });
+    const openai = new OpenAIApi(configuration);
     const stories = await scrapeGamespot();
-    response.type("html").send(stories.join("<br>"));
+
+    const regenerateResponses = stories.map(async (storyTitle) => {
+      return (
+        await openai.createCompletion({
+          model: "text-davinci-003",
+          prompt: `Please recreate the title of this article: "${storyTitle}" for a news site.`,
+        })
+      ).data.choices[0].text;
+    });
+
+    const newTitles = await Promise.all(regenerateResponses);
+    console.log(newTitles);
+    response.type("html").send(newTitles.join("<br>"));
   });
