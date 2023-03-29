6cdb398637eb2637121322654e8fffa539541474
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2023-03-29 16:16:59 +0300

    connect frontend to firestore

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '../global.css'
+import "../global.css";
+import { initializeApp } from "firebase/app";
+
+initializeApp({
+  apiKey: "AIzaSyBx6a0ht1KP3WJYELn_u_Q_M4ECwljwoSY",
+  authDomain: "articlegenius-ai.firebaseapp.com",
+  projectId: "articlegenius-ai",
+  storageBucket: "articlegenius-ai.appspot.com",
+  messagingSenderId: "676688590911",
+  appId: "1:676688590911:web:086a8d15fea3feb55d208a",
+});
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import styles from "../styles/home.module.css";
+import { doc, getFirestore, getDoc } from "firebase/firestore";
 
 function Home() {
-  return <main className={styles.main}>Hello world</main>;
+  const db = getFirestore();
+  useEffect(async () => {
+    const docRef = doc(db, "ProductionArticles", "fVSlPIyK0NfN0fLhoiBr");
+    const docSnap = await getDoc(docRef);
+    console.log(docSnap.data());
+  }, []);
+  return <main className={styles.main}>Watch out the latest articles:</main>;
 }
 
 export default Home;
