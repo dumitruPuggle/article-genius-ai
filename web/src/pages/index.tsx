ec0d84eccdc939e5a6cabab8faf2262c5b4c37da
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2023-04-03 22:42:31 +0300

    add logo trademark

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -16,15 +16,6 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.center}>
           <Logo />
-          <ul class="nav nav-pills nav-fill gap-2 p-1 small bg-primary rounded-5 shadow-sm" id="pillNav2" role="tablist" style={{'--bs-nav-link-color': 'var(--bs-white)', '--bs-nav-pills-link-active-color': 'var(--bs-primary)', '--bs-nav-pills-link-active-bg': 'var(--bs-white)'}}>
-            <li className="nav-item" role="presentation">
-              <button className="nav-link active rounded-5" id="home-tab2" data-bs-toggle="tab" type="button" role="tab" aria-selected="true">Home</button>
-            </li>
-          </ul>
-        </div>
-        <div className="w-100 center container p-4">
-          <h2>Check out the latest news:</h2>
-          <small className="text-auto">Check out the latest news:</small>
         </div>
       </main>
     </>
